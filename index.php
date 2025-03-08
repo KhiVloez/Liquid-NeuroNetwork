84735537e7cedab9f7948bce3ae6cd2a40b8b4f1
Arianna <165401844+BonzaiBlackout@users.noreply.github.com>
2025-03-08 06:22:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -20,12 +20,15 @@
 <script>
     document.getElementById('btn').addEventListener("click", function()
     {
+        // get value from text input 
+        const userInput = document.getElementById('userprompt').value;
+        // send user input to the api 
         fetch("http://127.0.0.1:5000/regbutton", {
             method: "POST",
             headers: {
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({message: "Hello from frontend!"})
+            body: JSON.stringify({message: userInput}) // input value will be in the string message
         })
         .then(response => resonse.json())
         .then(data => console.log("Success:", data))
